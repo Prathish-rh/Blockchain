3d506249c89bebc9110e931b1c3634e3fcce5f2a
PRATHISH <121566542+Prathish-rh@users.noreply.github.com>
2025-10-23 21:53:32 +0530

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+npm install @veramo/core @veramo/cli @veramo/did-manager @veramo/key-manager \
+@veramo/data-store @veramo/kms-local @veramo/did-provider-key \
+@veramo/credential-w3c typeorm sqlite3
